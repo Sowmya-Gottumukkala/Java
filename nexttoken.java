928972459c413296811334dd4c0af3c27813a7f4
Sowmya-Gottumukkala <127223176+Sowmya-Gottumukkala@users.noreply.github.com>
2024-11-02 10:12:09 +0530

    Update nexttoken.java

--- nexttoken.java
+++ nexttoken.java
@@ -1,1 +1,13 @@
 //printing next token 
+
+import java.util.*;
+class Main
+{
+  public static void main(String[] args)
+    {
+        StringTokenizer st1 = new StringTokenizer("Hello", " ");
+        while (st1.hasMoreTokens())
+              System.out.println(st1.nextToken());
+     
+    }
+}
